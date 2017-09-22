b12a4c809b78bb6cebd8ad7fbbc35e40957a9225
Brian Parrish <brian.parrish3739@gmail.com>
2017-09-22 19:03:30 -0400

    added some styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import Key from './Key'
-// import ReactDOM from "react-dom";
-// import KeyHandler, {KEYPRESS, KEYDOWN, KEYUP} from 'react-key-handler';
-// import keyHandler from './keyPress.js';
-// var clicked = false;
 
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var chunks = {
   0: [],
   1: [],
-  2: []
+  2: [],
+  3: [],
+  4: [],
+  5: []
 };
 var dest = audioCtx.createMediaStreamDestination();
 
@@ -27,7 +24,8 @@ class App extends Component {
     this.state = {
       recorders: [],
       current: -1,
-      duration: 0
+      duration: 0,
+      countdown: 3
     }
     this.addRecording = this.addRecording.bind(this);
     this.startRecording = this.startRecording.bind(this);
@@ -37,6 +35,27 @@ class App extends Component {
 
   setTime(time) {
     this.setState({duration: time})
+  }
+
+  playAll() {
+    let loops = document.getElementsByClassName("loop");
+    for(var loop of loops) {
+      loop.play();
+    }
+  }
+
+  pauseAll() {
+    let loops = document.getElementsByClassName("loop");
+    for(var loop of loops) {
+      loop.pause();
+    }
+  }
+
+  resetAll() {
+    let loops = document.getElementsByClassName("loop");
+    for(var loop of loops) {
+      loop.load();
+    }
   }
 
   addRecording() {
@@ -58,29 +77,43 @@ class App extends Component {
   }
 
   startRecording(recorder) {
-    if(this.state.duration === 0) recorder.start();
+    if(this.state.duration === 0) {
+      let interval1 = setInterval(() => {
+        this.setState({countdown: this.state.countdown-1})
+        if(this.state.countdown === 0) {
+          recorder.start();
+          clearInterval(interval1);
+        }
+      }, 1000)
+    }
     else {
       let timeLimit = this.state.duration*1000;
-      recorder.start()
-      setTimeout(() => {
-        this.stopRecording(recorder);
-      }, timeLimit)
+      let interval2 = setInterval(() => {
+        this.setState({countdown: this.state.countdown-1})
+        if(this.state.countdown === 0) {
+          recorder.start();
+          clearInterval(interval2);
+          setTimeout(() => {
+            this.stopRecording(recorder);
+          }, timeLimit)
+        } 
+      }, 1000)
     }
   }
 
   stopRecording(recorder) {
+    this.setState({countdown: 3});
     let id = this.state.recorders.indexOf(recorder);
     recorder.stop();
     recorder.ondataavailable = (event) => {
       chunks[id].push(event.data);
     }
-    
+
     recorder.onstop = (evt) => {
       let blob = new Blob(chunks[id], { 'type' : 'audio/ogg; codecs=opus' });
       var audioElem = document.getElementById(id);
       audioElem.src = URL.createObjectURL(blob);
       audioElem.loop = true;
-      
       audioElem.addEventListener('loadedmetadata', () => {
         this.setTime(audioElem.duration);
       })
@@ -91,81 +124,58 @@ class App extends Component {
     let recorders = this.state.recorders;
     return (
       <div>
-        <h1><strong>Arpegg.io</strong></h1>
-        <h3>RECORDER: </h3>
-        <br />
-        <button type="button" onClick={this.addRecording}>Click Add A New Recorder</button>
-        <br />
-        {
-          recorders.map(recorder => {
-            let id = this.state.recorders.indexOf(recorder);
-            return (
-                <div key={this.state.recorders.indexOf(recorder)}>
-                  <button type="button" onClick={() => this.startRecording(this.state.recorders[id])}>Record #{id+1}</button>
-                  <button type="button" onClick={() => this.stopRecording(this.state.recorders[id])}>Stop #{id+1}</button>
-                  <audio id={id} className="loop" controls></audio>
-                </div>
-            )
-          })
-        }
-        <br />
-        <button type="button" onClick={this.playAll}>Play All</button>
-        <button type="button" onClick={this.pauseAll}>Pause All</button>
+        <div id="img-holder">
+          <img id="img" src='http://cdn.pastemagazine.com/www/system/images/photo_albums/aditlo-grace-potter/large/photo_25322_0-37.jpg?1384968217' />
+        </div>
+        <div className="center container-fluid">
+          <div id="break"></div>
+          <div className="col-xs-2"></div>
+            <div className="col-xs-8">
+              <div id="arpeggio">
+                <text id="title"><strong>Arpegg.io</strong></text>
+              </div>
+              <div id="countdown-holder">
+                <text id="record-in">Record In</text>
+                  <div>
+                  {this.state.countdown === 0 
+                  ? <text className="three"><strong>Go</strong></text>
+                  : <text className="three"><strong>{this.state.countdown}</strong></text>
+                  }
+                  </div>
+                <button type="button" className="add-recording btn btn-primary"onClick={this.addRecording}>Add New Loop</button>
+              <br />
+              <br />
+              {
+                recorders.map(recorder => {
+                  let id = this.state.recorders.indexOf(recorder);
+                  return (
+                      <div key={this.state.recorders.indexOf(recorder)}>
+                        <div id="recording-buttons">
+                          <button type="button" className="start-stop btn btn-danger" onClick={() => this.startRecording(this.state.recorders[id])}>Record</button>
+                          { id === 0 
+                          ? <button type="button" className="start-stop btn btn-danger" onClick={() => this.stopRecording(this.state.recorders[id])}>Stop</button>
+                          : <button type="button" className="start-stop btn btn-danger" disabled>Auto-Stops</button>
+                          }
+                        </div>
+                        <audio id={id} className="loop" controls></audio>
+                        <br />
+                        <br />
+                      </div>
+                  )
+                })
+              }
+              <br />
+              <button type="button" onClick={this.playAll}>Play All</button>
+              <button type="button" onClick={this.pauseAll}>Pause All</button>
+              <button type="button" onClick={this.resetAll}>Reset All</button>
+              </div>
+            </div>
+          <div className="col-xs-2"></div>
+        </div>
       </div>
     );
   }
 }
-
-// createOctave= () => {
-//   let noteFreq = {};;
-//   noteFreq["C"] = 261.625565300598634;
-//   noteFreq["C#"] = 277.182630976872096;
-//   noteFreq["D"] = 293.664767917407560;
-//   noteFreq["D#"] = 311.126983722080910;
-//   noteFreq["E"] = 329.627556912869929;
-//   noteFreq["F"] = 349.228231433003884;
-//   noteFreq["F#"] = 369.994422711634398;
-//   noteFreq["G"] = 391.995435981749294;
-//   noteFreq["G#"] = 415.304697579945138;
-//   noteFreq["A"] = 440.000000000000000;
-//   noteFreq["A#"] = 466.163761518089916;
-//   noteFreq["B"] = 493.883301256124111;
-//   return noteFreq;
-// }
-//          onMouseDown={(event) => this.startNote(event, e, audioCtx)}
-// onMouseUp={(event) => this.stopNote(event, e, audioCtx)}
-
-// createNoteTable = () => {
-//   let noteFreq = [];
-//   for (let i=0; i< 9; i++) {
-//     noteFreq[i] = [];
-//   }
-//   noteFreq[4]["C"] = 261.625565300598634;
-//   noteFreq[4]["C#"] = 277.182630976872096;
-//   noteFreq[4]["D"] = 293.664767917407560;
-//   noteFreq[4]["D#"] = 311.126983722080910;
-//   noteFreq[4]["E"] = 329.627556912869929;
-//   noteFreq[4]["F"] = 349.228231433003884;
-//   noteFreq[4]["F#"] = 369.994422711634398;
-//   noteFreq[4]["G"] = 391.995435981749294;
-//   noteFreq[4]["G#"] = 415.304697579945138;
-//   noteFreq[4]["A"] = 440.000000000000000;
-//   noteFreq[4]["A#"] = 466.163761518089916;
-//   noteFreq[4]["B"] = 493.883301256124111;
-
-//   return noteFreq;
-// }
-
-// {
-//   Object.entries(octave).forEach(([note, freq]) => {
-//     return (
-//       <div>
-//         <h1>HELLO</h1>
-//         {/*<Key note={note} freq={freq} />*/}
-//       </div>
-//     )
-//   })
-// }
 
 export default App;
 
@@ -192,25 +202,6 @@ export default App;
 // mediaRecorder.ondataavailable = (event) => {
 //   console.log('data', event.data)
 //   chunks.push(event.data);
-// }
-
-
-// playAll() {
-//   this.state.recorders.forEach(recorder => {
-//     let loops = document.getElementsByClassName("loop");
-//     for(var i=0; i<loops.length; loops++) {
-//       if(!loops[i].loop) loops[i].loop = true;
-//     }
-//   })
-// }
-
-// pauseAll() {
-//   this.state.recorders.forEach(recorder => {
-//     let loops = document.getElementsByClassName("loop");
-//     for(var i=0; i<loops.length; loops++) {
-//       if(loops[i].loop) loops[i].loop = false;
-//     }
-//   })
 // }
 
 // mediaRecorder.onstop = (evt) => {
@@ -285,3 +276,58 @@ export default App;
 // <KeyHandler keyEventName={KEYUP} keyValue="d" onKeyHandle={(event) => this.stopNote(event, g, audioCtx)} />
 // {<button type="button">G</button>}
 // </div>
+
+
+
+
+
+// createOctave= () => {
+//   let noteFreq = {};;
+//   noteFreq["C"] = 261.625565300598634;
+//   noteFreq["C#"] = 277.182630976872096;
+//   noteFreq["D"] = 293.664767917407560;
+//   noteFreq["D#"] = 311.126983722080910;
+//   noteFreq["E"] = 329.627556912869929;
+//   noteFreq["F"] = 349.228231433003884;
+//   noteFreq["F#"] = 369.994422711634398;
+//   noteFreq["G"] = 391.995435981749294;
+//   noteFreq["G#"] = 415.304697579945138;
+//   noteFreq["A"] = 440.000000000000000;
+//   noteFreq["A#"] = 466.163761518089916;
+//   noteFreq["B"] = 493.883301256124111;
+//   return noteFreq;
+// }
+//          onMouseDown={(event) => this.startNote(event, e, audioCtx)}
+// onMouseUp={(event) => this.stopNote(event, e, audioCtx)}
+
+// createNoteTable = () => {
+//   let noteFreq = [];
+//   for (let i=0; i< 9; i++) {
+//     noteFreq[i] = [];
+//   }
+//   noteFreq[4]["C"] = 261.625565300598634;
+//   noteFreq[4]["C#"] = 277.182630976872096;
+//   noteFreq[4]["D"] = 293.664767917407560;
+//   noteFreq[4]["D#"] = 311.126983722080910;
+//   noteFreq[4]["E"] = 329.627556912869929;
+//   noteFreq[4]["F"] = 349.228231433003884;
+//   noteFreq[4]["F#"] = 369.994422711634398;
+//   noteFreq[4]["G"] = 391.995435981749294;
+//   noteFreq[4]["G#"] = 415.304697579945138;
+//   noteFreq[4]["A"] = 440.000000000000000;
+//   noteFreq[4]["A#"] = 466.163761518089916;
+//   noteFreq[4]["B"] = 493.883301256124111;
+
+//   return noteFreq;
+// }
+
+// {
+//   Object.entries(octave).forEach(([note, freq]) => {
+//     return (
+//       <div>
+//         <h1>HELLO</h1>
+//         {/*<Key note={note} freq={freq} />*/}
+//       </div>
+//     )
+//   })
+// }
