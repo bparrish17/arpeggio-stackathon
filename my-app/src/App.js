a1019a1c46158bebcb42a49b99b0f7a5c3dc5ff0
Brian Parrish <brian.parrish3739@gmail.com>
2017-11-01 17:02:50 -0400

    added comments for readability, removed notes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,6 +18,7 @@ var dest = audioCtx.createMediaStreamDestination();
 //create a synth and connect it to the master output (your speakers)
 let test = synth.toMaster();
 
+//create drum tone for metronome
 var drum = new Tone.MembraneSynth({
   "pitchDecay" : 0.008,
   "octaves" : 2,
@@ -28,10 +29,6 @@ var drum = new Tone.MembraneSynth({
   }
 }).toMaster();
 
-// Tone.Transport.stop()
-
-//play a middle 'C' for the duration of an 8th note
-//test.triggerAttackRelease("C4", "8n");
 
 navigator.getUserMedia = navigator.getUserMedia ||
 navigator.webkitGetUserMedia ||
@@ -88,6 +85,8 @@ class App extends Component {
   addRecording() {
     console.log('COUNTDOWN', this.state.countdown)
     console.log('DURATION', this.state.duration)
+
+    //Create new Recorder for Audio Destination
     if (navigator.mediaDevices) {
       console.log('getUserMedia supported.');
         navigator.getUserMedia({audio: true}, 
@@ -145,7 +144,6 @@ class App extends Component {
     // 2) First recording, metronomeSet: metronome countdown
     // 3) Second --> no timer
 
-    
     if(this.state.duration <= 0) {
       let interval1 = setInterval(() => {
         this.setState({countdown: this.state.countdown-1})
@@ -258,180 +256,3 @@ class App extends Component {
 
 export default App;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// onMouseUp={() => this.endNote(node)}
-    // let gainNode = audioCtx.createGain();
-    // gainNode.connect(audioCtx.destination);
-
-
-
-    //<Key note={note} freq={freq} />
-
-
-    //USER MEDIA RECORDING
-
-
-    // <audio id="recording1" controls></audio>
-    // <br />
-    // <audio id="recording2" controls></audio>
-    // <br />
-    // <audio id="recording3" controls></audio>
-    // <br />
-
-// mediaRecorder.ondataavailable = (event) => {
-//   console.log('data', event.data)
-//   chunks.push(event.data);
-// }
-
-// mediaRecorder.onstop = (evt) => {
-//   // Make blob out of our blobs, and open it.
-//   var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
-//   // let testBlob = URL.createObjectURL(blob);
-//   // let myAudio = new Audio(testBlob); 
-//   // myAudio.loop = true;
-//   // myAudio.play();
-
-//   document.querySelector("audio").src = URL.createObjectURL(blob);
-//   document.querySelector("audio").loop = true;
-// };
-
-        // console.log("METRONOME COUNT/STATE DURATION", metronomeCount);
-        // console.log('BEAT COUNTDOWN', beatCountdown);
-        // console.log("STATE COUNTDOWN", this.state.countdown);
-
-// <button type="button" onClick={this.addRecording}>Click Add A New Recorder</button>
-// <button type="button" onClick={() => this.startRecording(this.state.recorders[1])}>Record 2</button>
-// <button type="button" onClick={() => this.stopRecording(this.state.recorders[1])}>Stop 2</button>
-// <button type="button" onClick={() => this.startRecording(this.state.recorders[2])}>Record 3</button>
-// <button type="button" onClick={() => this.stopRecording(this.state.recorders[2])}>Stop 3</button>
-
-
-// let c = audioCtx.createOscillator();
-// c.type = 'triangle';
-// c.frequency.value = 261.625565300598634;
-// c.start();
-
-
-// let e = audioCtx.createOscillator();
-// e.type = 'triangle';
-// e.frequency.value = 329.627556912869929;;
-// e.start();
-
-// let g = audioCtx.createOscillator();
-// g.type = 'triangle';
-// g.frequency.value = 391.995435981749294;
-// g.start();
-
-// this.startNote = this.startNote.bind(this);
-// this.stopNote = this.stopNote.bind(this);
-
-
-// startNote(event, node, audioCtx) {
-//   node.connect(audioCtx.destination);
-//   node.connect(dest);
-// }
-
-// stopNote(event, node, audioCtx) {
-//   node.disconnect(audioCtx.destination);
-//   node.disconnect(dest);
-// }
-
-// startRecording() {
-// mediaRecorder.start();
-// }
-
-// stopRecording() {
-// mediaRecorder.stop();
-// }
-
-//<div id="C4">
-// <KeyHandler keyEventName={KEYDOWN} keyValue="a" onKeyHandle={(event) => this.startNote(event, c, audioCtx)} />
-// <KeyHandler keyEventName={KEYUP} keyValue="a" onKeyHandle={(event) => this.stopNote(event, c, audioCtx)} />
-// {<button type="button">C</button>}
-// </div>
-// <div id="E4">
-// <KeyHandler keyEventName={KEYDOWN} keyValue="s" onKeyHandle={(event) => this.startNote(event, e, audioCtx)} />
-// <KeyHandler keyEventName={KEYUP} keyValue="s" onKeyHandle={(event) => this.stopNote(event, e, audioCtx)} />
-// {<button type="button">E</button>}
-// </div>
-// <div id="G4">
-// <KeyHandler keyEventName={KEYDOWN} keyValue="d" onKeyHandle={(event) => this.startNote(event, g, audioCtx)} />
-// <KeyHandler keyEventName={KEYUP} keyValue="d" onKeyHandle={(event) => this.stopNote(event, g, audioCtx)} />
-// {<button type="button">G</button>}
-// </div>
-
-
-
-
-
-// createOctave= () => {
-//   let noteFreq = {};;
-//   noteFreq["C"] = 261.625565300598634;
-//   noteFreq["C#"] = 277.182630976872096;
-//   noteFreq["D"] = 293.664767917407560;
-//   noteFreq["D#"] = 311.126983722080910;
-//   noteFreq["E"] = 329.627556912869929;
-//   noteFreq["F"] = 349.228231433003884;
-//   noteFreq["F#"] = 369.994422711634398;
-//   noteFreq["G"] = 391.995435981749294;
-//   noteFreq["G#"] = 415.304697579945138;
-//   noteFreq["A"] = 440.000000000000000;
-//   noteFreq["A#"] = 466.163761518089916;
-//   noteFreq["B"] = 493.883301256124111;
-//   return noteFreq;
-// }
-//          onMouseDown={(event) => this.startNote(event, e, audioCtx)}
-// onMouseUp={(event) => this.stopNote(event, e, audioCtx)}
-
-// createNoteTable = () => {
-//   let noteFreq = [];
-//   for (let i=0; i< 9; i++) {
-//     noteFreq[i] = [];
-//   }
-//   noteFreq[4]["C"] = 261.625565300598634;
-//   noteFreq[4]["C#"] = 277.182630976872096;
-//   noteFreq[4]["D"] = 293.664767917407560;
-//   noteFreq[4]["D#"] = 311.126983722080910;
-//   noteFreq[4]["E"] = 329.627556912869929;
-//   noteFreq[4]["F"] = 349.228231433003884;
-//   noteFreq[4]["F#"] = 369.994422711634398;
-//   noteFreq[4]["G"] = 391.995435981749294;
-//   noteFreq[4]["G#"] = 415.304697579945138;
-//   noteFreq[4]["A"] = 440.000000000000000;
-//   noteFreq[4]["A#"] = 466.163761518089916;
-//   noteFreq[4]["B"] = 493.883301256124111;
-
-//   return noteFreq;
-// }
-
-// {
-//   Object.entries(octave).forEach(([note, freq]) => {
-//     return (
-//       <div>
-//         <h1>HELLO</h1>
-//         {/*<Key note={note} freq={freq} />*/}
-//       </div>
-//     )
-//   })
-// }
